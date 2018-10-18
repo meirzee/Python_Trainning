5752d07817a4aec9051524598f2c6327e673dc56
Meir Zeevi <meirzee@amdocs.com>
2018-10-18 11:11:38 +0300

    .

--- Excercise1.py
+++ Excercise1.py
@@ -1,4 +1,4 @@
-#Birthday
+#Birthday calculation
 import sys
 from datetime import date
 from datetime import time
@@ -9,13 +9,16 @@ from datetime import timedelta
 NAME = input("What is your name? ")
 AGE = int(input("How Old are you? "))
 DELTA = (100 - AGE)
+NUMBER = int(input("Please specify message number: "))
 
 TODAY = date.today()
 
 
 DESTINATION_DATE= TODAY.replace(year = TODAY.year + DELTA)
 print ("Hello ", NAME ,"you are now " , AGE ,"years old and you have got " , DELTA , "years left to live up to 100 years old." )
-print ("You will be 100 years old on:", DESTINATION_DATE )
+for i in range(NUMBER):
+    print ("You will be 100 years old on:", DESTINATION_DATE )
+
 
 
 
